cb92c39ba52c86797c154867c2eea768b32c7a67
sunchao <sunchao5@jd.com>
2018-10-30 16:16:19 +0800

    init commit, test

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # format-tools
-the formatter with multiple function for javascript
+一个简单的格式化工具；包含格式化日期时间、数字、字符串的几个常用方法；
+
+## 安装
+### Node
+```js   
+$ npm install format-tools
+```
+### Browser
+```html
+<script src="path/format-tools.js"></script>
+```
+
+## 用法
+### 引用
+```js
+import formatTools from 'format-tools';
+```
+
+### format(str, args)
+一个简单的字符串格式化工具，支持下标和属性两种方式，属性名称为数字、字母和下划线的组合；
+- str：模板字符串
+```js
+  formatTools.format('{0}-{1}-{2}', 2018, '08', 10); // => '2018-08-10'
+  formatTools.format('{year}-{month}-{day}', {year:2018, month:'08', day: 10}); // => '2018-08-10'
+```
+
+### formatDate(date, splitChar)
+格式化日期，返回日期部分；有两个参数：
+- date:date|number|string，支持日期对象、时间戳；
+- splitChar: 可选，日期的分隔符号，默认为 '-'
+```js
+  formatTools.formatDate(new Date(2018, 9, 10, 12, 34, 56)); // => '2018-10-10'
+  formatTools.formatDate(1539146096000, '/'); // => '2018/10/10'
+```
+
+### formatTime(date)
+格式化日期，返回时间部分；一个参数：
+- date:date|number|string，支持日期对象、时间戳；
+```js
+  formatTools.formatTime(new Date(2018, 9, 10, 12, 34, 56)); // => '12:34:56'
+  formatTools.formatTime(1539146096000); // => '12:34:56'
+```
+
+### formatDateTime(date, splitChar)
+格式化日期时间，返回日期+时间；有两个参数：
+- date:date|number|string，支持日期对象、时间戳；
+- splitChar: 可选，日期的分隔符号，默认为 '-'
+```js
+  formatTools.formatDateTime(new Date(2018, 9, 10, 12, 34, 56)); // => '2018-10-10 12:34:56'
+  formatTools.formatDateTime(1539146096000, '/'); // => '2018/10/10 12:34:56'
+```
+
+### formatByThousand(num)
+格式化数字，金额用到的时候较多，三位加一个千分位符号，参数：
+- num: number|string，需要格式化的数字；
+```js
+  formatTools.formatByThousand(1234567); // => '1,234,567'
+  formatTools.formatDateTime('1234.56'); // => '1,234.56'
+  1234.56.toLocaleString('en-us'); // => '1,234.56'
+```
+
+### split4(str)
+格式化字符串，四位加一个空格，银行卡号用到的较多；
+- str: number|string，需要格式化的字符串；
+```js
+  formatTools.split4(6225880112345678); // => '6225 8801 1234 5678'
+```
+
+### getType(args)
+获取参数类型字符串(小写)；
+- args: String|Object|Number|Date|null|undefined，需要格式化的字符串；
+```js            
+    formatTools.getType(new Date()); // => 'date'
+    formatTools.getType(123); // => 'number'
+    formatTools.getType('test'); // => 'string'
+    formatTools.getType({a: 1}); // => 'object'
+    formatTools.getType(null);  // => 'null'
+    formatTools.getType(undefined); // => 'undefined'
+```
---
+++ dist/index.js
@@ -0,0 +1,106 @@
+'use strict';
+
+/**
+ * 获取变量的类型
+ * @param {*} [args] - 需要获取类型的变量
+ * @returns {string} 变量的类型字符串 string/object/number/date/null/undefined
+ * */
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function getType(args) {
+  var type = Object.prototype.toString.call(args);
+  var result = type.replace(/\[object\s+(\w+)\]/gmi, "$1");
+  return result.toLowerCase();
+}
+
+/**
+ * 格式化字符串
+ * @param {string} [str] - 需要格式化的字符串
+ * @example
+ * // returns '2018-10-12'
+ * format('{0}-{1}-{2}', 2018, 10, 12);
+ * @example
+ * // returns '2018-10-13'
+ * format('{year}-{month}-{day}', {year:2018, month:10, day: 13});
+ * @returns {string} 格式化好的日期和时间字符串
+ * */
+function format(str) {
+  var pattern = /\{([\w_]+)\}/gm;
+  var arr = Array.prototype.slice.call(arguments, 1);
+  var args = /\{(\d+)\}/.test(str) ? arr : arr[0];
+  return str.replace(pattern, function () {
+    return args[arguments[1]];
+  });
+}
+
+/**
+ * 格式化日期
+ * @param {(date|number|string)} [date=new Date()] - 需要格式化的日期
+ * @param {string} [splitChar='-'] - 日期（年月日）之间的分隔符
+ * @returns {string} 格式化好的日期字符串
+ * */
+function formatDate(date, splitChar) {
+  var datetime = formatDateTime(date, splitChar);
+  return datetime.split(' ')[0];
+}
+
+/**
+ * 格式化时间
+ * @param {(date|number|string)} [date=new Date()] - 需要格式化的日期
+ * @returns {string} 格式化好的时间字符串
+ * */
+function formatTime(date) {
+  var datetime = formatDateTime(date);
+  return datetime.split(' ')[1];
+}
+
+/**
+ * 格式化日期时间
+ * @param {(date|number|string)} [date=new Date()] - 需要格式化的日期
+ * @param {string} [splitChar='-'] - 日期（年月日）之间的分隔符
+ * @returns {string} 格式化好的日期和时间字符串
+ * */
+function formatDateTime(date, splitChar) {
+  date = date || new Date();
+  splitChar = splitChar || '-';
+
+  var type = getType(date);
+  if (type === "number") date = new Date(date);else if (type === 'string') date = new Date(parseInt(date));else if (type !== 'date') date = new Date();
+
+  var str = date.toString(); // Sun Apr 01 2018 21:57:48 GMT+0800 (CST)
+
+  // 正则匹配出所有需要的数据
+  var res = str.replace(/\w+\s+(\w+)\s+(\d{2})\s+(\d{4})\s+(\d{2}):(\d{2}):(\d{2})[\s\S]+/gmi, '$3--$2 $4:$5:$6'); // 2018--04-01 12:04:05
+  // 月份是英文缩写，所以直接用 getMonth 方法获取到，根据数字的位数判断是否要添加0
+  var m = (date.getMonth() + 1).toString().replace(/^(\d)$/gmi, '0$1').replace(/^(\d+)$/gmi, '-$1-');
+  res = res.replace(/--/gmi, m);
+  if (splitChar && splitChar !== '-') res = res.replace(/-/gmi, splitChar);
+
+  return res;
+}
+
+/**
+ * 格式化数字
+ * @param {(number|string)} [num] - 需要格式化数字
+ * @returns {string} 格式化好的数字
+ * */
+function formatByThousand(num) {
+  num = parseFloat(num);
+  if (isNaN(num)) throw new Error('请传入有效的数字');
+  return num.toLocaleString('en-US');
+}
+
+/**
+ * 格式化字符串，四位加一个空格
+ * @param {(number|string)} [str] - 需要格式化字符串
+ * @returns {string} 格式化好的字符串
+ * */
+function split4(str) {
+  return str.toString().replace(/\s+/gmi, "").replace(/(.{4})/gmi, "$1 ").replace(/\s+$/gmi, "");
+}
+
+var mod = { getType: getType, format: format, formatDate: formatDate, formatTime: formatTime, formatDateTime: formatDateTime, split4: split4, formatByThousand: formatByThousand };
+
+exports.default = mod;
